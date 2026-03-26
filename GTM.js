4273d6ff4c7b40a76fc1a9a50f29717a93f2bcff
Samyak Jain <115931952+samyckk@users.noreply.github.com>
2026-03-26 12:46:26 +0530

    new try for GMT

--- GTM.js
+++ GTM.js
@@ -60,6 +60,29 @@
 // })();
 
 
-console.log("UUID123456123456")
+$(function () {
+
+  // 🚀 Inject GTM in <head>
+  (function () {
+    var gtmScript = document.createElement("script");
+    gtmScript.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-N5WJ95Q9');`;
+
+    document.head.appendChild(gtmScript);
+  })();
+
+  // 🚀 Inject GTM <noscript> right after <body>
+  (function () {
+    var noscript = document.createElement("noscript");
+    noscript.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N5WJ95Q9"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
+
+    document.body.insertBefore(noscript, document.body.firstChild);
+  })();
+
+});
 
 
