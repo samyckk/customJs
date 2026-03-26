f229ec147b2bbe5fda29f6208ae4a2ffe24f6055
Samyak Jain <115931952+samyckk@users.noreply.github.com>
2026-03-26 14:01:23 +0530

    new GTM

--- GTM.js
+++ GTM.js
@@ -1,37 +1,27 @@
-$(document).ready(function () {
+if (location.pathname === "/moas/") {
 
-  // Check if URL starts with /moas/
-  if (location.pathname.startsWith("/moas/")) {
+  // 1. Add GTM script in <head>
+  (function(w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
 
-    // duplicate injection (important)
-    if (window.gtmInjected) return;
-    window.gtmInjected = true;
+    var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
 
-    // 1. Inject GTM script into <head>
-    (function (w, d, s, l, i) {
-      w[l] = w[l] || [];
-      w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+    j.async = true;
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
 
-      var f = d.getElementsByTagName(s)[0],
-          j = d.createElement(s),
-          dl = l !== 'dataLayer' ? '&l=' + l : '';
-
-      j.async = true;
-      j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-
-      f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', 'GTM-N5WJ95Q9');
+    f.parentNode.insertBefore(j, f);
+  })(window, document, 'script', 'dataLayer', 'GTM-N5WJ95Q9');
 
 
-    // 2. Inject noscript right after <body>
-    var noscriptTag = `
-      <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N5WJ95Q9"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe>
-      </noscript>
-    `;
+  // 2. Add noscript immediately after <body>
+  var noscript = document.createElement('noscript');
+  noscript.innerHTML = `
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N5WJ95Q9"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe>
+  `;
 
-    $("body").prepend(noscriptTag);
-  }
-
-});
+  document.body.insertBefore(noscript, document.body.firstChild);
+}
