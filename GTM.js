cfbd9fa1f86ffd835d1c11e5d9300435488b34b2
Samyak Jain <115931952+samyckk@users.noreply.github.com>
2026-03-26 12:39:59 +0530

    Checking console.log

--- GTM.js
+++ GTM.js
@@ -1,59 +1,65 @@
-(function () {
+// (function () {
 
-    var GTM_ID = "GTM-N5WJ95Q9";
+//     var GTM_ID = "GTM-N5WJ95Q9";
 
-    // Add GTM script to <head>
+//     // Add GTM script to <head>
 
-    function addGTMToHead() {
+//     function addGTMToHead() {
 
-        var script = document.createElement("script");
+//         var script = document.createElement("script");
 
-        script.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+//         script.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+// new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+// j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+// 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 
-})(window,document,'script','dataLayer','${GTM_ID}');`;
+// })(window,document,'script','dataLayer','${GTM_ID}');`;
 
-        document.head.appendChild(script);
+//         document.head.appendChild(script);
 
-    }
+//     }
 
-    //Add noscript iframe to <body>
+//     //Add noscript iframe to <body>
 
-    function addNoScriptToBody() {
+//     function addNoScriptToBody() {
 
-        var noscript = document.createElement("noscript");
+//         var noscript = document.createElement("noscript");
 
-        noscript.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
+//         noscript.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
 
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
+// height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
 
-        function insert() {
+//         function insert() {
 
-            document.body.insertBefore(noscript, document.body.firstChild);
+//             document.body.insertBefore(noscript, document.body.firstChild);
 
-        }
+//         }
 
-        if (document.body) {
+//         if (document.body) {
 
-            insert();
+//             insert();
 
-        } else {
+//         } else {
 
-            document.addEventListener("DOMContentLoaded", insert);
+//             document.addEventListener("DOMContentLoaded", insert);
 
-        }
+//         }
 
-    }
+//     }
 
-    // Execute both
+//     // Execute both
 
-    addGTMToHead();
+//     addGTMToHead();
 
-    addNoScriptToBody();
 
-})();
+
+//     addNoScriptToBody();
+// })();
+
+
+console.log("UUID123456123456")
+
+
